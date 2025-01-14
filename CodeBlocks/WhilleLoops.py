011028b2d50620234a617f0ef0b39fd8fe0e54b0
Dmitry Kuznets <dimkuz18@gmail.com>
2025-01-14 13:52:28 +0200

    While Loops

---
+++ CodeBlocks/WhilleLoops.py
@@ -0,0 +1,18 @@
+#Reeborg World Hurdle2 Challange Answer:
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+while not at_goal():
+    jump()
