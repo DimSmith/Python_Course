266b634d802126b20d75d5978f2a67b20a4beb89
Dmitry Kuznets <dimkuz18@gmail.com>
2025-01-18 11:20:46 +0200

    Functions

---
+++ Functions/functions.py
@@ -0,0 +1,14 @@
+def greet():
+    print("Hello")
+    print("How are you?")
+    print("Greetings")
+
+greet()
+
+def greet_name(name):
+    print(f"Hello {name}")
+    print(f"How are you {name}?")
+    print(f"Greetings {name}")
+
+greet_name("Dima")
+
