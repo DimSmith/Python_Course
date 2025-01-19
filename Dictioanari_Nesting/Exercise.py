bf46c0cf1a5ac9310f763e584aaa832ac1da8a73
Dmitry Kuznets <dimkuz18@gmail.com>
2025-01-19 16:51:03 +0200

    Exercise

---
+++ Dictioanari_Nesting/Exercise.py
@@ -0,0 +1,21 @@
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+
+student_grades = student_scores
+
+for key in student_grades:
+    if student_grades[key] >= 91:
+        student_grades[key] = "Outstanding"
+    elif student_grades[key] >= 81:
+        student_grades[key] = "Exceeds Expectations"
+    elif student_grades[key] >= 71:
+        student_grades[key] = "Acceptable"
+    else:
+        student_grades[key] = "Fail"
+
+print(student_grades)
